d8f3b5c68b389e76b3f4303ce766e0f0c4976065
kade-cooper <112003731+kade-cooper@users.noreply.github.com>
2023-03-29 12:26:13 -0400

    added player and rope

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector2 moveVec = new Vector2();
+        moveVec.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        moveVec.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        Vector2 newPosition = this.transform.position + new Vector3(moveVec.x, moveVec.y, 0f);
+        this.transform.position = newPosition;
+    }
+}
