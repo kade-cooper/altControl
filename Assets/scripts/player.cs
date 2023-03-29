4bb4e11d169c933406bf18f0eccda7e5db9ef4e0
kade-cooper <112003731+kade-cooper@users.noreply.github.com>
2023-03-29 19:43:02 -0400

    added rope (needs work)

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public float speed;
+    public Rigidbody2D ropeEnd;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +19,6 @@ public class player : MonoBehaviour
         moveVec.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         Vector2 newPosition = this.transform.position + new Vector3(moveVec.x, moveVec.y, 0f);
         this.transform.position = newPosition;
+        ropeEnd.AddForce(new Vector2(-moveVec.x,0f));
     }
 }
