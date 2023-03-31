32e6118b2de7d932ba5ef76953f08450bc18e553
kade-cooper <112003731+kade-cooper@users.noreply.github.com>
2023-03-31 16:32:29 -0400

    fixed rope physics

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     public float speed;
+    public float vertSpeed;
     public Rigidbody2D ropeEnd;
+    public Rigidbody2D playerR2D;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +18,11 @@ public class player : MonoBehaviour
     {
         Vector2 moveVec = new Vector2();
         moveVec.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        moveVec.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        Vector2 newPosition = this.transform.position + new Vector3(moveVec.x, moveVec.y, 0f);
+        moveVec.y = Input.GetAxis("Vertical") * vertSpeed * Time.deltaTime;
+        Vector2 force = new Vector3(moveVec.x, 0f, 0f);
+        Vector2 newPosition = this.transform.position + new Vector3(0f, moveVec.y, 0f);
         this.transform.position = newPosition;
-        ropeEnd.AddForce(new Vector2(-moveVec.x,0f));
+        playerR2D.AddForce(force);
+        ropeEnd.AddForce(new Vector2(-moveVec.x*0.2f,0f));
     }
 }
