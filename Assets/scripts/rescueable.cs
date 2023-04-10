cfadf97a468e5883bd1f32d2e2a79e3e1052e8ef
kade-cooper <112003731+kade-cooper@users.noreply.github.com>
2023-04-10 00:02:19 -0400

    Revert "Revert "added capacity text""
    This reverts commit 608b0e40d40e7336ad6d24505f6dfffe258f1c29.

--- Assets/scripts/rescueable.cs
+++ Assets/scripts/rescueable.cs
@@ -1,19 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class rescueable : MonoBehaviour
 {
     private GameObject player;
+    public TextMeshProUGUI capacity;
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.CompareTag("ropeEnd")){
             player.SendMessage("enablePerson");
             Destroy(this.gameObject, 0f);
+            Debug.Log(capacity);
+        }
+        else if(other.gameObject.CompareTag("ropeEndDisabled")){
+            capacity.text="AT CAPACITY";
         }
     }
-    void Update(){
+    private void OnCollisionExit2D(Collision2D other){
+        if(other.gameObject.CompareTag("ropeEndDisabled")){
+            capacity.text="";
+        }
+    }
+    void Start(){
         player = GameObject.FindWithTag("player");
+        Debug.Log(capacity);
+        //capacitytxt = text.Find("atCapacity");
     }
 }
